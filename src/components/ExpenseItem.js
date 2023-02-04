c8c1c2c9493c3ab748bca892c2bc3f6f5f216334
pardeepky <pardeep.pky@gmail.com>
2023-02-05 02:07:14 +0530

    making smaller components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import './ExpenseItem.css';
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem = ({LocationOfExpenditure, title, amount, date}) => {
-
-    return (
-        <div className='expense-item'>
-            <div>{date.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h2>{title}</h2><h2>{LocationOfExpenditure}</h2>
-                <div className='expense-item__price'>Rs {amount}</div>
-            </div>
-        </div>
-    )
+const ExpenseItem = (props) => {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails amount={props.amount} title={props.title} />
+    </div>
+  );
 }
 
 export default ExpenseItem

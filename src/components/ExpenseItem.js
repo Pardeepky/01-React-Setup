95ced1ae9cd0d487f570a9af9791318bef887326
pardeepky <pardeep.pky@gmail.com>
2023-02-05 01:12:15 +0530

    added data with varibales

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+    const expenseDate = new Date(2023, 2, 5);
+    const expenseTitle = 'Food';
+    const expenseAmount = 10;
+    const LocationOfExpenditure = 'Delhi'
+
     return (
-        <div>
-            <h3>Expense Item!</h3>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
+        <div className='expense-item'>
+            <div>{expenseDate.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2><h2>{LocationOfExpenditure}</h2>
+                <div className='expense-item__price'>Rs {expenseAmount}</div>
+            </div>
         </div>
     )
 }

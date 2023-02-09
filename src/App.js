73b0af00d0d0b95256a96a49c7e12e72069083d9
pardeepky <pardeep.pky@gmail.com>
2023-02-09 13:20:37 +0530

    added a placeholder to filter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import Expenses from "./components/Expense/Expenses";
-import ExpenseForm from "./components/Expense/ExpenseForm";
+import ExpenseForm from "./components/NewExpense/ExpenseForm";
 import { useState } from "react";
-import "./components/Expense/NewExpense.css";
+import "./components/NewExpense/NewExpense.css";
+
+const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
 const App = () => {
-  const [expensesData, setExpensesData] = useState([]);
+  const [expensesData, setExpensesData] = useState(expenses);
 
   const addExpenseHandler = (expense) => {
     let newExpense = {
-      ...expense, id: Math.random().toString()
+      ...expense, id: Math.random().toString(), date: new Date(expense.date.split('-'))
     }
     setExpensesData(prev => [...prev, newExpense]);
   };
--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,35 +1,20 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from "react";
 
-const Expenses = ({expensesData}) => {
-  console.log("exp",expensesData);
-  // const expenses = [
-  //   {
-  //     id: "e1",
-  //     title: "Toilet Paper",
-  //     amount: 94.12,
-  //     date: new Date(2020, 7, 14),
-  //   },
-  //   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  //   {
-  //     id: "e3",
-  //     title: "Car Insurance",
-  //     amount: 294.67,
-  //     date: new Date(2021, 2, 28),
-  //   },
-  //   {
-  //     id: "e4",
-  //     title: "New Desk (Wooden)",
-  //     amount: 450,
-  //     date: new Date(2021, 5, 12),
-  //   },
-  // ];
+const Expenses = ({ expensesData }) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {expensesData.map((element) => {
-        console.log(element);
         return (
           <ExpenseItem
             key={element.id}
